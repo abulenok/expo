be2416da58c79751b2f4cba58822219228bb59ff
Oleksii Bulenok <oleksii.bulenok@swmansion.com>
2025-03-21 16:01:55 +0100

    Figure out prod build

--- tools/src/commands/PublishProdExpoHomeCommand.ts
+++ tools/src/commands/PublishProdExpoHomeCommand.ts
@@ -175,15 +175,16 @@ async function action(): Promise<void> {
   console.log('Getting expo-cli state of the current session...');
   const cliState = await getExpoCliStateAsync();
   const cliUsername = cliState?.auth?.username;
-  if (cliUsername !== 'exponent') {
-    throw new Error('Must be logged in as `exponent` account to publish');
-  }
+  console.error(cliUsername);
+  // if (cliUsername !== 'exponent') {
+  //   throw new Error('Must be logged in as `exponent` account to publish');
+  // }
 
   const appJsonFilePath = path.join(EXPO_HOME_PATH, 'app.json');
 
-  const slug = 'home';
-  const owner = 'exponent';
-  const easProjectId = '6b6c6660-df76-11e6-b9b4-59d1587e6774';
+  const slug = 'vibecode-expogo';
+  const owner = 'vibecodeexpogo';
+  const easProjectId = 'c4955a97-0d95-4f69-aedb-9355603ce575';
   const easUpdateURL = `https://u.expo.dev/${easProjectId}`;
 
   const appJsonFile = new JsonFile<AppConfig>(appJsonFilePath);
